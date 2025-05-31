7b23a7373474d7cff44399d037eae4cb58d7b151
Evan Kline Mores <21-03567@g.batstate-u.edu.ph>
2025-05-31 12:08:20 +0800

    Finish 'Learn Introductory JavaScript by Building a Pyramid Generator'

--- pyramid_generator.js
+++ pyramid_generator.js
@@ -1,18 +1,18 @@
-const character = "#";
-const count = 8;
+const character = "!";
+const count = 10;
 const rows = [];
+let inverted = false;
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
-
-if ("false") {
-  console.log("Condition is true");
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
 }
 
 let result = ""
