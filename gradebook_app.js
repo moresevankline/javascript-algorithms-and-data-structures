d24c7fe381b301fb8d84788cc638e97e25310ac8
Evan Kline Mores <21-03567@g.batstate-u.edu.ph>
2025-06-04 11:02:34 +0800

    Apply Prettier - Code formatter

--- gradebook_app.js
+++ gradebook_app.js
@@ -32,9 +32,23 @@ function studentMsg(totalScores, studentScore) {
   const average = getAverage(totalScores);
   const grade = getGrade(studentScore);
   if (hasPassingGrade(studentScore)) {
-    return "Class average: " + average + ". " + "Your grade: " + grade + ". You passed the course."
+    return (
+      "Class average: " +
+      average +
+      ". " +
+      "Your grade: " +
+      grade +
+      ". You passed the course."
+    );
   } else {
-    return "Class average: " + average + ". " + "Your grade: " + grade + ". You failed the course."
+    return (
+      "Class average: " +
+      average +
+      ". " +
+      "Your grade: " +
+      grade +
+      ". You failed the course."
+    );
   }
 }
 console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
--- pyramid_generator.js
+++ pyramid_generator.js
@@ -4,7 +4,11 @@ const rows = [];
 let inverted = false;
 
 function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
 }
 
 for (let i = 1; i <= count; i++) {
@@ -15,7 +19,7 @@ for (let i = 1; i <= count; i++) {
   }
 }
 
-let result = ""
+let result = "";
 
 for (const row of rows) {
   result = result + row + "\n";
