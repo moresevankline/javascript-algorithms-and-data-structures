3f31c3b1297fd0b90c723291a82f2ea9a0032c11
Evan Kline Mores <21-03567@g.batstate-u.edu.ph>
2025-06-07 14:42:50 +0800

    Continue building Calorie Counter and manage to make it till Step 62

--- calorie_counter/script.js
+++ calorie_counter/script.js
@@ -21,8 +21,26 @@ function addEntry() {
     `#${entryDropdown.value} .input-container`
   );
   const entryNumber =
-    targetInputContainer.querySelectorAll('input[type="text"]').length;
+    targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
   const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />`;
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+  targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
+
+function getCaloriesFromInputs(list) {
+  let calories = 0;
+  const invalidInputMatch = isInvalidInput(currVal);
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+  }
+}
+
+addEntryButton.addEventListener("click", addEntry);
