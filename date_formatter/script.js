e6caf21464c22408e4b617c9fe324979fd87fa83
Evan Kline Mores <21-03567@g.batstate-u.edu.ph>
2025-06-12 19:39:57 +0800

    Start 'Learn the Date Object by Building a Date Formatter' and manage to make it till Step 22

---
+++ date_formatter/script.js
@@ -0,0 +1,21 @@
+const currentDateParagraph = document.getElementById("current-date");
+const dateOptionsSelectElement = document.getElementById("date-options");
+
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
+const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate;
+
+const exampleSentence = "selur pmaCedoCeerf".split("").reverse().join("");
+console.log(exampleSentence);
+dateOptionsSelectElement.addEventListener("change", () => {
+  switch (dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate;
+  }
+});
